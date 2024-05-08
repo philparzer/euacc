5015a723c42c4253cb9b42c35dce33bafe0f2bdd
philparzer <parzerphilipp@gmail.com>
2024-05-08 19:57:28 +0200

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ import WaitlistForm from "@/components/waitlist-form";
 export default function Home() {
   return (
     <main className="flex relative z-10 min-h-[100dvh] pt-[100px] flex-col items-center justify-center">
-      <section className="flex flex-col text-center  max-w-[600px] gap-2 items-center">
-        <h1 className="text-2xl sm:text-3xl font-bold leading-normal sm:leading-[1.5em] text-white ">
-          Let&apos;s make <span className="underline underline-offset-2 decoration-eu-yellow decoration-5">Europe</span>{" "}
+      <section className="flex flex-col text-center  max-w-[600px] 2xl:max-w-[800px] gap-2 items-center">
+        <h1 className="text-2xl sm:text-4xl 2xl:text-5xl font-bold text-white">
+          <span className="leading-[1.4em]">
+          Let&apos;s make <span className="underline underline-offset-2 decoration-eu-yellow decoration-5 ">Europe</span>{" "}
           <span className="bg-eu-yellow rounded-md px-1 text-black whitespace-nowrap">
             the center of the world
           </span>
-          again
+          {" "}again
+          </span>
         </h1>
         <p className="mt-1 sm:mt-2 text-lg sm:text-xl">
           EU/Accelerationism is a movement for centrist ideas that enable rapid
